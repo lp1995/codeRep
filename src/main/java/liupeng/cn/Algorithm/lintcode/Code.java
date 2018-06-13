d29ac56bcf8ba7c46fbeadbdeb75f92d415cbeee
liupeng <liupengfun@163.com>
2018-06-13 22:14:49 +0800

    123

--- src/main/java/liupeng/cn/Algorithm/lintcode/Code.java
+++ src/main/java/liupeng/cn/Algorithm/lintcode/Code.java
@@ -9,37 +9,54 @@ import java.util.Stack;
 
 public class Code {
 
-	public boolean hasCycle(ListNode head) {
-		// write your code here
-		ListNode h1 = head,h2=head;
-		while(h2!=null){
-			h1=h1.next;
-			h2=h2.next;
-			if(h2==null){
-				return false;
-			}
-			h2=h2.next;
-			if(h2==null){
-				return false;
-			}
-			if(h1.val==h2.val){
-				return true;
-			}
+	public TreeNode sortedListToBST(ListNode head) {
+        
+		TreeNode out = sort(head,null);
+		return out;
+    }
+	
+	public TreeNode sort(ListNode head, ListNode end){
+		if(head==null){
+			return null;
 		}
-		return false;
+		if(head==end){
+			return null;
+		}
+		ListNode a=head;
+		ListNode b=head;
+		boolean flag = true;
+		while(a.next!=end){
+			a=a.next;
+			if(flag){
+				b=b.next;
+			}
+			flag=!flag;
+		}
 		
-		
+		TreeNode node = new TreeNode(b.val);
+		TreeNode left = sort(head,b);
+		TreeNode right = sort(b.next,end);
+		node.left = left;
+		node.right = right;
+		return node;
 	}
-
+	
+	
 	public static void main(String... arg) {
 		Code code = new Code();
-		ListNode l1 = new ListNode(1);
-		ListNode l2 = new ListNode(2);
-		ListNode l3 = new ListNode(3);
-		ListNode l4 = new ListNode(4);
-		l1.next = l2;
-		l2.next = l3;
-		l3.next = l4;
+		ListNode l1  = new ListNode(1);
+		ListNode l2  = new ListNode(2);
+		ListNode l3  = new ListNode(3);
+		ListNode l4  = new ListNode(4);
+		ListNode l5  = new ListNode(5);
+		ListNode l6  = new ListNode(6);
+		l1.next=l2;
+		l2.next=l3;
+		l3.next=l4;
+		l4.next=l5;
+		l5.next=l6;
+		TreeNode a= code.sortedListToBST(l1);
+		System.out.println(a);
 
 	}
 
---
+++ src/main/java/liupeng/cn/Algorithm/lintcode/RandomListNode.java
@@ -0,0 +1,10 @@
+package liupeng.cn.Algorithm.lintcode;
+
+public class RandomListNode {
+	int label;
+	RandomListNode next, random;
+
+	RandomListNode(int x) {
+		this.label = x;
+	}
+}
