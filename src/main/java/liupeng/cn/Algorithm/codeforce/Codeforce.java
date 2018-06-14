4be4fd8f6d0879eeb58db93f821382bae50f14db
liupeng <liupengfun@163.com>
2018-06-14 20:30:11 +0800

    123

--- src/main/java/liupeng/cn/Algorithm/codeforce/Codeforce.java
+++ src/main/java/liupeng/cn/Algorithm/codeforce/Codeforce.java
@@ -7,52 +7,85 @@ import java.util.Scanner;
 public class Codeforce {
 	public static void main(String ...arg){
 		Scanner in = new Scanner(System.in);
-		int len=in.nextInt();
-		int p = in.nextInt();
-		String s = in.next();
-		char []a = s.toCharArray();
-		boolean flag=true;
-		for(int i=0;i<len-p;i++){
-			if(a[i]=='.'&&a[i+p]=='.'){
-				a[i]='1';
-				a[i+p]='0';
-				flag =false;
-			}else if(a[i]=='.'&&a[i+p]!='.'){
-				if(a[i+p]=='1'){
-					a[i]='0';
-				}else{
-					a[i]='1';
-				}
-				flag =false;
-			}else if(a[i]!='.'&&a[i+p]=='.'){
-				if(a[i]=='1'){
-					a[i+p]='0';
-				}else{
-					a[i+p]='1';
-				}
-				flag =false;
-			}else{
-				if(a[i]!=a[i+p]){
-					flag = false;
-				}
+		int a = in.nextInt()-1;
+		int b =in.nextInt()-1;
+		int c=in.nextInt()-1;
+		int d = in.nextInt()-1;
+		int m=36;
+		int n = 50;
+		char s[][] = new char[m][n];
+		//make a 
+		for(int i=0;i<9;i++){
+			for(int j=0;j<n;j++){
+				s[i][j]='A';
 			}
 		}
-		int b = len/p;
-		int c=len%p;
-		for(int i=len-p*b;i<len-c;i++){
-			if(a[i]=='.'){
-				a[i]='0';
+		//make b
+		for(int i=9;i<18;i++){
+			for(int j=0;j<n;j++){
+				s[i][j]='B';
+			}
+		}
+		//make c
+		for(int i=18;i<27;i++){
+			for(int j=0;j<n;j++){
+				s[i][j]='C';
+			}
+		}
+		//make d
+		for(int i=27;i<36;i++){
+			for(int j=0;j<n;j++){
+				s[i][j]='D';
+			}
+		}
+		//use a
+		int x=10,y=0;
+		for(int i=0;i<a;i++){
+			s[x][y]='A';
+			y+=2;
+			if(y>48){
+				y=0;
+				x+=2;
 			}
 			
 		}
-		String out = "";
-		for(int i=0;i<len;i++){
-			out+=a[i];
+		//use b
+		x=19;y=0;
+		for(int i=0;i<b;i++){
+			s[x][y]='B';
+			y+=2;
+			if(y>48){
+				y=0;
+				x+=2;
+			}
 		}
-		if(flag==true){
-			System.out.println("No");
-		}else{
-			System.out.println(out);
+		//use c
+		x=28;y=0;
+		for(int i=0;i<c;i++){
+			s[x][y]='C';
+			y+=2;
+			if(y>48){
+				y=0;
+				x+=2;
+			}
 		}
+		//use d
+		x=1;y=0;
+		for(int i=0;i<d;i++){
+			s[x][y]='D';
+			y+=2;
+			if(y>48){
+				y=0;
+				x+=2;
+			}
+		}
+		System.out.println(m+" "+n);
+		for(int i=0;i<m;i++){
+			for(int j=0;j<n;j++){
+				System.out.print(s[i][j]);
+			}
+			System.out.println();
+		}
+
 	}
 }
--- src/main/java/liupeng/cn/Algorithm/lintcode/Code.java
+++ src/main/java/liupeng/cn/Algorithm/lintcode/Code.java
@@ -9,55 +9,28 @@ import java.util.Stack;
 
 public class Code {
 
-	public TreeNode sortedListToBST(ListNode head) {
-        
-		TreeNode out = sort(head,null);
-		return out;
-    }
-	
-	public TreeNode sort(ListNode head, ListNode end){
-		if(head==null){
-			return null;
-		}
-		if(head==end){
-			return null;
-		}
-		ListNode a=head;
-		ListNode b=head;
-		boolean flag = true;
-		while(a.next!=end){
-			a=a.next;
-			if(flag){
-				b=b.next;
-			}
-			flag=!flag;
-		}
+	public ListNode deleteDuplicates(ListNode head) {
+        // write your code here
 		
-		TreeNode node = new TreeNode(b.val);
-		TreeNode left = sort(head,b);
-		TreeNode right = sort(b.next,end);
-		node.left = left;
-		node.right = right;
-		return node;
-	}
-	
-	
+    }
+
 	public static void main(String... arg) {
 		Code code = new Code();
-		ListNode l1  = new ListNode(1);
-		ListNode l2  = new ListNode(2);
-		ListNode l3  = new ListNode(3);
-		ListNode l4  = new ListNode(4);
-		ListNode l5  = new ListNode(5);
-		ListNode l6  = new ListNode(6);
+		ListNode l1 = new ListNode(1);
+		ListNode l2 = new ListNode(1);
+		ListNode l3 = new ListNode(11);
+		ListNode l4 = new ListNode(15);
+		ListNode l5 = new ListNode(22);
 		l1.next=l2;
 		l2.next=l3;
 		l3.next=l4;
 		l4.next=l5;
-		l5.next=l6;
-		TreeNode a= code.sortedListToBST(l1);
-		System.out.println(a);
-
+		ListNode out = code.deleteDuplicates(l1);
+		while(out!=null){
+			System.out.println(out.val);
+			out=out.next;
+		}
+		
 	}
 
 }
